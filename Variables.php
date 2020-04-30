c62553ad7941d667a8fe2376a606749e7bbf75c4
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-04-30 20:16:16 +0530

    DAY 1

---
+++ Variables.php
@@ -0,0 +1,29 @@
+<?php
+
+//do not put space after the $sign , type the variable name
+
+$value = 25;//numerical variable
+$name ="Programming Php"; //string variable
+
+$value =  ($value*8);
+
+//directly accessing the value
+echo "$name";
+
+//directly accessing the value
+echo "$value";
+
+//adding multiplication
+echo "$value*8";
+
+//print the both values
+echo "$name,$value";
+
+//passing the value
+echo "25*8 =",$value;
+
+//the value is taken literally
+echo '25*8=$value';
+
+
+?>
---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+//first comment in the php
+/*
+this is the hello world code in php
+*/
+
+#this is also the comment
+
+
+echo " Hello <br>
+<b>World<b>";
+
+?> 
